3d8609997618520b1617df3505df0f4bb1297aa9
rifaq-ahmer <rifaq.ahmer@gmail.com>
2021-03-19 22:27:03 +0530

    till-img-modal-dislplay

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import DisplayImages from "./DisplayImages";
 import axios from "axios";
 import "../index.css";
 
 const Home = () => {
 	const [images, setImages] = useState();
+	const [show, setShow] = useState();
+
+	const handleClick = (e) => {
+		setShow(e);
+
+
 	useEffect(() => {
 		const getData = async () => {
 			const { data: Images } = await axios.get(
@@ -17,14 +24,23 @@ const Home = () => {
 		getData();
 	}, []);
 	return (
-		<div className="gallery">
-			{images &&
-				images.map((e) => (
-					<>
-						<img src={e.urls.raw} alt="images" />
-					</>
-				))}
-		</div>
+		<>
+			<div className="gallery">
+				{images &&
+					images.map((e) => (
+						<>
+							<img
+								onClick={() => handleClick(e.urls.raw)}
+								data-toggle="modal"
+								data-target="#exampleModalCenter"
+								src={e.urls.raw}
+								alt="images"
+							/>
+						</>
+					))}
+			</div>
+			<DisplayImages Src={show} />
+		</>
 	);
 };
 
